e4eb20c45178636ed2c40ced6bbf7bb3a2c80acb
Mohammad <javakarans@gmail.com>
2017-04-30 10:45:53 +0430

    bean.java controller.java

---
+++ src/main/java/com/coffeeshop/bean/bean.java
@@ -0,0 +1,7 @@
+package com.coffeeshop.bean;
+
+/**
+ * Created by Mohammad on 4/30/2017.
+ */
+public class bean {
+}
---
+++ src/main/java/com/coffeeshop/controller/controller.java
@@ -0,0 +1,7 @@
+package com.coffeeshop.controller;
+
+/**
+ * Created by Mohammad on 4/30/2017.
+ */
+public class controller {
+}
