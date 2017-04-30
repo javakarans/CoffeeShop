60244665ab3fcf43fe4af2cf982cebf5caec69be
H&H <hooman.kaltokal@gmail.com>
2017-04-30 15:35:05 +0430

    create admin edit menu  (bean)

---
+++ src/main/java/com/coffeeshop/bean/AdminEditMenu.java
@@ -0,0 +1,19 @@
+package com.coffeeshop.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by H&H on 4/30/2017.
+ */
+@ManagedBean
+@SessionScoped
+public class AdminEditMenu {
+
+    @PostConstruct
+    private void init(){
+
+
+    }
+}
---
+++ src/main/java/com/coffeeshop/bean/AdminLogin.java
@@ -0,0 +1,7 @@
+package com.coffeeshop.bean;
+
+/**
+ * Created by H&H on 4/30/2017.
+ */
+public class AdminLogin {
+}
