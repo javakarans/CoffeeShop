4b31332efc987f53ee4a6b281678be1275dd13dc
Mohammad <javakarans@gmail.com>
2017-05-01 18:35:23 +0430

    Design

---
+++ src/main/webapp/lib/js/categoryPage.js
@@ -0,0 +1,19 @@
+$(function(){
+    var down=false;
+    var scrollLeft=0;
+    var x = 0;
+
+    $('.scrollable').mousedown(function(e) {
+        down = true;
+        scrollLeft = this.scrollLeft;
+        x = e.clientX;
+    }).mouseup(function() {
+        down = false;
+    }).mousemove(function(e) {
+        if (down) {
+            this.scrollLeft = scrollLeft + x - e.clientX;
+        }
+    }).mouseleave(function() {
+        down = false;
+    });
+})
