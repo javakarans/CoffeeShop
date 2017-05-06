2fbe0419236ab70cc70aa63aece86ce20985daba
Amirhossein <javakarans@gmail.com>
2017-05-06 08:06:37 -0700

    kitchen

--- src/main/java/com/coffeeshop/bean/Kitchen.java
+++ src/main/java/com/coffeeshop/bean/Kitchen.java
@@ -1,11 +1,18 @@
 package com.coffeeshop.bean;
 
 import com.coffeeshop.database.KitchenDaoImp;
+import com.coffeeshop.database.KitchenOrderTempDao;
+import com.coffeeshop.database.KitchenOrderTempDaoImp;
+import com.coffeeshop.model.KitchenOrderTemp;
+import com.coffeeshop.wrapper.KitchenOrderTempWrapper;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Amirhossein on 5/4/2017.
@@ -14,12 +21,27 @@ import java.io.Serializable;
 @SessionScoped
 public class Kitchen implements Serializable{
 
-    private KitchenDaoImp kitchenDaoImp;
-    private FoodOrderWrapper foodOrderWrapper;
+    private KitchenOrderTempDao kitchenOrderTempDaoImp;
+    private List<KitchenOrderTemp> kitchenOrderTemps;
+    private List<KitchenOrderTempWrapper> kitchenOrderTempsWrapper;
+    private HashMap<String, String> urlParam;
+    private long kitchenId;
 
     @PostConstruct
-    public void init(){
-        kitchenDaoImp = new KitchenDaoImp();
+    public void init() {
+        kitchenOrderTempDaoImp = new KitchenOrderTempDaoImp();
+        kitchenId = getUrlParam();
+        kitchenOrderTemps = kitchenOrderTempDaoImp.getKitchenOrderTempByKitchenId(kitchenId);
+        for (int i = 0; i < kitchenOrderTemps.size(); i++){
+            KitchenOrderTempWrapper kitchenOrderTempWrapper = new KitchenOrderTempWrapper(kitchenOrderTemps.get(i));
+            kitchenOrderTempsWrapper.add(kitchenOrderTempWrapper);
+        }
+
+    }
+
+    public long getUrlParam(){
+        urlParam = (HashMap<String, String>) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        return  Long.parseLong(urlParam.get("kitchenId"));
     }
 
 }
