c1e68f1dad394fa45deeb9edd0cb4e921c9c3389
Mohammad <javakarans@gmail.com>
2017-06-20 20:04:02 +0430

    Update

--- src/main/java/com/coffeeshop/services/GmailSMTP.java
+++ src/main/java/com/coffeeshop/services/GmailSMTP.java
@@ -1,8 +1,17 @@
 package com.coffeeshop.services;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -39,23 +48,41 @@ public class GmailSMTP {
         return session;
     }
 
-    public void sendMessage(String toAddress,String subject,StringBuilder text){
+    public void sendMessage(String toAddress, String subject, StringBuilder text,List<File> attachmentList){
         Message message=new MimeMessage(gmailSession);
+        BodyPart messageBodyPart = new MimeBodyPart();
+        Multipart multipart = new MimeMultipart();
         try {
             message.setFrom(new InternetAddress(gmailAddress));
             message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(toAddress));
             message.setSubject(subject);
+            messageBodyPart.setText(text.toString());
+            multipart.addBodyPart(messageBodyPart);
             message.setText(text.toString());
+            if(!attachmentList.isEmpty()){
+                Iterator<File> iterator = attachmentList.iterator();
+                while (iterator.hasNext()){
+                    File attachment = iterator.next();
+                    messageBodyPart=new MimeBodyPart();
+                    DataSource dataSource=new FileDataSource(attachment);
+                    messageBodyPart.setDataHandler(new DataHandler(dataSource));
+                    messageBodyPart.setFileName(attachment.getName());
+                    multipart.addBodyPart(messageBodyPart);
+                }
+            }
+            message.setContent(multipart);
             Transport.send(message);
             System.out.println("Done");
+
         } catch (MessagingException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        GmailSMTP gmailSMTP=new GmailSMTP("","");
-        StringBuilder stringBuilder=new StringBuilder("");
-        gmailSMTP.sendMessage("","",stringBuilder);
+        GmailSMTP gmailSMTP=new GmailSMTP("nutellapluserbil","nutellaplus0000");
+        StringBuilder stringBuilder=new StringBuilder("Nutella Plus Test Gmail");
+        List<File> files=new ArrayList<>();
+        gmailSMTP.sendMessage("mmirzakhani1993@gmail.com","Test4",stringBuilder,files);
     }
 }
